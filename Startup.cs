c9ef2c988f89bf54346a3aa02b88746a28a5a6b7
wowhy <wowhy@outlook.com>
2016-10-29 23:52:08 +0800

    调整日志

--- Startup.cs
+++ Startup.cs
@@ -40,11 +40,11 @@ namespace AspnetcoreVue
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            // loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            // loggerFactory.AddDebug();
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging"));
                 app.UseDeveloperExceptionPage();
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
                 {
