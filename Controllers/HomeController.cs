57390a9987b48e7ecc4b59d7bca6fa3f4c312af7
wowhy <wowhy@outlook.com>
2016-10-30 22:59:24 +0800

    修改package.json

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AspnetcoreVue.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new object {});
         }
 
         public IActionResult Error()
